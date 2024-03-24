7d541dfa098e819adf27abfdf258678aeffe71e5
Damon <cman55245@gmail.com>
2024-03-25 02:56:08 +0530

    Final Touch

---
+++ src/app/(pages)/signup/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import useAuth from "@/context/useAuth";
+import { useRouter } from "next/navigation";
+import React from "react";
+import Signup from "@/components/SignUp";
+
+
+
+const SignUp = () => {
+    const router = useRouter();
+    const {authStatus} = useAuth();
+
+    if(authStatus) {
+        router.replace("/profile");
+        return <></>;
+    }
+
+    return (
+        <section className="px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+            <Signup />
+        </section>
+    )
+}
+
+export default Signup;
---
+++ src/context/useAuth.ts
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import AuthContext from "./authContext";
+
+
+const useAuth = () => {
+    const data = useContext(AuthContext);
+    return data;
+}
+
+
+
+
+export default useAuth;
